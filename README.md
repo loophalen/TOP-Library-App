cc45e6dac033f34b386ddd4b4378eb00f55bd02e
Austin Harris <abharris@outlook.com>
2022-01-29 13:21:00 -0500

    reading data from form field

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # TOP-Library-App
-# Library-App
--- main.js
+++ main.js
@@ -1,12 +1,61 @@
-let myLibrary = ["What is the What", "The Every", "The Circle"];
+const bookDisplay = document.querySelector("#book-item");
+
+//button to open pop-up form
+const openFormBtn = document.querySelector("#open-form");
+openFormBtn.addEventListener("click", openForm);
+
+//buton to close pop-up form
+const closeBtn = document.querySelector(".btn-close");
+closeBtn.addEventListener("click", closeForm);
+
+//accessing the values in the add book form
+const bookTitle = document.forms["form-container"]["book-title"];
+
+let bookAuthor = document.forms["form-container"]["book-author"].value;
+
+let bookPages = document.forms["form-container"]["book-pages"].value;
+
+//add button to add new book from pop-up form
+const addBook = document.querySelector(".btn-add");
+addBook.addEventListener("click", addBookToLibrary);
+
+const addBtn = document.querySelector(".btn-add");
+
+addBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const bookTitleValue = bookTitle.value;
+  console.log(bookTitleValue);
+});
+
+let myLibrary = [];
 
 function Book() {
   //the constructor
-  myLibrary.forEach((book) => console.log(book));
+  myLibrary.forEach((book) => {
+    const bookCard = document.createElement("h1");
+    bookCard = textContent = book;
+    bookDisplay.appendChild(bookCard);
+  });
 }
 
 function addBookToLibrary() {
   //add books
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.info = function () {
+    return `${title} by ${author}, ${pages} pages`;
+  };
+}
+
+Book();
+
+function openForm() {
+  document.getElementById("book-popup").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("book-popup").style.display = "none";
 }
 
 console.log(myLibrary);
