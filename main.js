043aa1bf6e2bc9db7ea65e6779f88c4d97f8ec78
Austin Harris <abharris@outlook.com>
2022-01-29 12:41:48 -0500

    created add button and form

--- main.js
+++ main.js
@@ -1,37 +1,12 @@
-let myLibrary = [];
-
-const bookDisplay = document.querySelector("#book-item");
-const openFormBtn = document.querySelector("#open-form");
-openFormBtn.addEventListener("click", openForm);
-const closeBtn = document.querySelector(".btn-close");
-closeBtn.addEventListener("click", closeForm);
+let myLibrary = ["What is the What", "The Every", "The Circle"];
 
 function Book() {
   //the constructor
-  myLibrary.forEach((book) => {
-    const bookCard = document.createElement("h1");
-    bookCard.textContent = book;
-    bookDisplay.appendChild(bookCard);
-  });
+  myLibrary.forEach((book) => console.log(book));
 }
 
-// object constructor
-function addBookToLibrary(title, author, pages) {
+function addBookToLibrary() {
   //add books
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.info = function () {
-    return `${title} by ${author}, ${pages} pages`;
-  };
 }
 
-Book();
-
-function openForm() {
-  document.getElementById("book-form").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("book-form").style.display = "none";
-}
+console.log(myLibrary);
